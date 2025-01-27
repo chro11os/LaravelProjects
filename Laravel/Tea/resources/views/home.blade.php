a026be76566a659ce02db9798853ba80ee293726
cro11os <neilbragsg@gmail.com>
2025-01-27 19:06:11 +0800

    update readme

--- Laravel/Tea/resources/views/home.blade.php
+++ Laravel/Tea/resources/views/home.blade.php
@@ -5,7 +5,8 @@
     <meta name="viewport"
           content="width=device-width, user-scalable=no, initial-scale=1.0, maximum-scale=1.0, minimum-scale=1.0">
     <meta http-equiv="X-UA-Compatible" content="ie=edge">
-    <link rel="stylesheet" href="{{ asset('/css/app.css') }}">
+    <link href="{{ asset('/css/app.css') }}" rel="stylesheet">
+    <script src="{{ asset('/js/app.js') }}"></script>
 
     <!-- Font -->
     <link rel="preconnect" href="https://fonts.googleapis.com">
@@ -16,6 +17,8 @@
     </head>
 <body>
     <h1>Hello Laravel</h1>
-
+    <h2>Howd you do??</h2>
+    <h3>ur user and</h3>
+    <p>My name is neil</p>
 </body>
 </html>
