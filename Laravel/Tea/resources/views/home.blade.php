90448278945288f603aaad64b7b81a27b1faeb9c
chrollos <106138109+chro11os@users.noreply.github.com>
2025-02-05 07:43:10 +0800

    finally found where to put css public/css/style.css

--- Laravel/Tea/resources/views/home.blade.php
+++ Laravel/Tea/resources/views/home.blade.php
@@ -5,15 +5,21 @@
     <meta name="viewport"
           content="width=device-width, user-scalable=no, initial-scale=1.0, maximum-scale=1.0, minimum-scale=1.0">
     <meta http-equiv="X-UA-Compatible" content="ie=edge">
-    <title>Home</title>
-    <h1> Tea </h1>
+    <!-- Modify css in public/css/style.css, not in css/app.css-->
+    <link rel="stylesheet" type="text/css" href="css/style.css">
 
-    <a href="/login" class="btn"> Login </a>
-    <a href="/register" class="btn"> Register </a>
-    <a href="/forgot-password" class="btn"> Forgot Password? </a>
-    
+    <!-- Font -->
+    <link rel="preconnect" href="https://fonts.googleapis.com">
+    <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
+    <link href="https://fonts.googleapis.com/css2?family=Exo+2:ital,wght@0,100..900;1,100..900&display=swap" rel="stylesheet">
+
+    <title>Home</title>
 </head>
 <body>
+    <h1> Tea </h1>
+
+    <a href="{{ route('password.request') }}" class="btn"> Forgot Password? </a>
+    <a href="{{ route('login') }}" class="btn"> Login </a>
 
 </body>
 </html>
